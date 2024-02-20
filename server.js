aba32d646482f717629f8d7b8a0482f9ecdc7c09
panda <p4nd4@posteo.de>
2024-02-20 11:00:00 +0100

    init server

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+// ----- middleware -----
+app.use(express.json());
+app.use(cors());
+
+// ----- routes ------
+app.post('/api/login', (req, res) => {
+  console.log(req.body.username, req.body.password);
+  //res.status(403).send('forbidden');
+  res.send('ok');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
